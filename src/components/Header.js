66a277ed19604a7e61cd97a07be674223c1117e2
Sougata01 <sougatadas01011999@gmail.com>
2024-07-20 14:36:59 +0530

    Implement_the_Home_page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,33 @@
+import { useLocation } from "react-router-dom";
 import NavBar from "./NavBar";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 const Header = (props) => {
+
+const location = useLocation()
+
     return (
         <header>
-            <NavBar onShow={props.onShow}/>
-                <h1 style={{fontFamily: "'Montserrat', sans-serif", marginTop: "60px", padding: "20px", fontSize: "70px"}} className="bg-secondary text-white text-center">The Generics</h1>
+            <NavBar onShow={props.onShow} />
+            <Container fluid className="bg-secondary text-white text-center">
+                <h1
+                    style={{
+                        fontFamily: "'Montserrat', sans-serif",
+                        marginTop: "60px", padding: "20px",
+                        fontSize: "70px"
+                    }}
+                >
+                    The Generics
+                </h1>
+                {location.pathname === '/' &&
+                <Row>
+                    <Col>
+                        <Button className="bg-secondary border border-info m-3">Get our Latest Album</Button>
+                    </Col>
+                    <svg className="mb-3" xmlns="http://www.w3.org/2000/svg" height="60px" viewBox="0 -960 960 960" width="48px" fill="#e8eaed"><path d="m383-310 267-170-267-170v340Zm97 230q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80Zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140Zm0-340Z"/></svg>
+                </Row>
+                }
+            </Container>
         </header>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,80 @@
 import React from 'react'
+import { Button, Col, Container, Row, Table, } from 'react-bootstrap'
+
+// JUL16
+// DETROIT, MI
+// DTE ENERGY MUSIC THEATRE
+// BUY TICKETS
+// JUL19
+// TORONTO,ON
+// BUDWEISER STAGE
+// BUY TICKETS
+// JUL 22
+// BRISTOW, VA
+// JIGGY LUBE LIVE
+// BUY TICKETS
+// JUL 29
+// PHOENIX, AZ
+// AK-CHIN PAVILION
+// BUY TICKETS
+// AUG 2
+// LAS VEGAS, NV
+// T-MOBILE ARENA
+// BUY TICKETS
+// AUG 7
+// CONCORD, CA
+// CONCORD PAVILION
+// BUY TICKETS
 
 function HomePage() {
   return (
-    <div>Home</div>
+    <Container>
+      <h3 className='text-center my-4'>TOURS</h3>
+      <Row className="d-flex justify-content-center">
+        <Col lg={8}>
+        <Table className='mb-5' hover>
+          <tbody>
+            <tr>
+              <td>JUL16</td>
+              <td>DETROIT, MI</td>
+              <td>DTE ENERGY MUSIC THEATRE</td>
+              <td className='text-center'><Button>BUY TICKETS</Button></td>
+            </tr>
+            <tr>
+              <td>JUL19</td>
+              <td>TORONTO,ON</td>
+              <td>BUDWEISER STAGE</td>
+              <td className='text-center'><Button>BUY TICKETS</Button></td>
+            </tr>
+            <tr>
+              <td>JUL 22</td>
+              <td>BRISTOW, VA</td>
+              <td>BiJIGGY LUBE LIVErd</td>
+              <td className='text-center'><Button>BUY TICKETS</Button></td>
+            </tr>
+            <tr>
+              <td>JUL16</td>
+              <td>DETROIT, MI</td>
+              <td>DTE ENERGY MUSIC THEATRE</td>
+              <td className='text-center'><Button>BUY TICKETS</Button></td>
+            </tr>
+            <tr>
+              <td>JUL19</td>
+              <td>TORONTO,ON</td>
+              <td>BUDWEISER STAGE</td>
+              <td className='text-center'><Button>BUY TICKETS</Button></td>
+            </tr>
+            <tr>
+              <td>JUL 22</td>
+              <td>BRISTOW, VA</td>
+              <td>BiJIGGY LUBE LIVErd</td>
+              <td className='text-center'><Button>BUY TICKETS</Button></td>
+            </tr>
+          </tbody>
+        </Table>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
